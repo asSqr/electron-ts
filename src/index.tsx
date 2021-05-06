6800e8b586b335394c2e512a1046ec0fb75bca52
asSqr <ryo.yossy@live.jp>
2021-05-06 15:01:38 +0900

    4th Day Completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import UserForm from './components/UserForm';
 import Store from './Store';
 
 const container = document.getElementById('contents');
 
-ReactDOM.render(
-  <Provider store={Store}>
-    <UserForm />
-  </Provider>,
-  container,
-);
+ReactDOM.render(<Provider store={Store}></Provider>, container);
